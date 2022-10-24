154eca75d0977d1e2a464ec9bfaa201306f34387
oliviams <35803567+oliviams@users.noreply.github.com>
2022-10-24 14:21:51 +0100

    Update axelrod_tournaments.py

--- axelrod_tournaments.py
+++ axelrod_tournaments.py
@@ -1,5 +1,7 @@
 import axelrod
 
+# Strategies included: TitForTat, TitFor2Tats, Cooperator, Defector, HardTitForTat, Alternator, TwoTitsForTat, Bully, Cycler, CyclerCCCD, CyclerDDC, Desperate, FirmButFair, Helpless, Willing, UsuallyCooperates, UsuallyDefects
+
 # Example using TitFotTat vs. Alternator
 strategies = [axelrod.TitForTat(), axelrod.Alternator()]
 match = axelrod.Match(strategies, 20)
